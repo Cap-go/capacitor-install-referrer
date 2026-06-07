420ea95f3cb48b0837d25d75eb848d148fbcaf53
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 13:13:37 +0200

    style: format capgo example rollout

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -3,19 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import pkg from './package.json';
 
 const config: CapacitorConfig = {
-  "appId": "app.capgo.installreferrer.example",
-  "appName": "Install Referrer Example",
-  "webDir": "dist",
-  "plugins": {
-    "CapacitorUpdater": {
-      "appId": "app.capgo.installreferrer.example",
-      "autoUpdate": true,
-      "autoSplashscreen": true,
-      "directUpdate": "always",
-      "defaultChannel": "production",
-      "version": pkg.version
-    }
-  }
+  appId: 'app.capgo.installreferrer.example',
+  appName: 'Install Referrer Example',
+  webDir: 'dist',
+  plugins: {
+    CapacitorUpdater: {
+      appId: 'app.capgo.installreferrer.example',
+      autoUpdate: true,
+      autoSplashscreen: true,
+      directUpdate: 'always',
+      defaultChannel: 'production',
+      version: pkg.version,
+    },
+  },
 };
 
 export default config;
